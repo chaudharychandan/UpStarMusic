0b329e8880b170cd50ced7a953b7f8a79bd08f23
Chandan Kumar <chandanchaudharyrules@gmail.com>
2018-03-12 01:43:17 +0530

    Create Artist yearsActive range query

--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,23 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  const minYearsActiveQuery = prepareYearsActiveQuery(1);
+  const maxYearsActiveQuery = prepareYearsActiveQuery(-1);
+
+  return Promise.all([minYearsActiveQuery, maxYearsActiveQuery])
+    .then((years) => {
+      return {
+        min: years[0],
+        max: years[1]
+      };
+    });
 };
+
+function prepareYearsActiveQuery(order) {
+  return Artist
+    .find({})
+    .sort({ yearsActive: order })
+    .limit(1)
+    .then((artist) => artist[0].yearsActive)
+  ;
+}
